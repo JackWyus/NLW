0b6fb5319c9829ed2fb4502c9f67886cb947a8fc
Vinicios Macario <JackWyus_01@outlook.com>
2022-06-03 16:33:06 -0300

    Corrigindo bug do Menu e Adicionando o botão para subi

--- js/main.js
+++ js/main.js
@@ -1,15 +1,42 @@
+
+window.addEventListener('scroll',onScroll)
+onScroll()
+
+
+
 /*Função responsável por selecionar o elemento html e adicionar a class de nome 'scroll' para que possa ser adicionado no css as devidas propriedades*/          
  /*A função vai verificar se 'scrollY' é maior que zero, se sim, ela vai adicionar a classList 'scroll' no html, se não, ela remove a classList 'scroll' - scrollY vai retornar(number) a posição da scroll no eixo y*/
-function onScroll(){
-    if(scrollY > 0){
-        document.querySelector('#navigation').classList.add('scroll')  
+
+
+
+/*A Function onScroll possui a função de gerenciar as outras Function que possuem relação com a Scrolly do navegador*/
+    function onScroll(){
+        showNavOnScroll();
+        showBackToTopButtonOnScroll();
     }
-    else{
-        document.querySelector('#navigation').classList.remove('scroll')  
+
+/*A Function showNavOnScroll possui a responsabilidade de mostrar o menu expandido ao movimentar o scrolly */
+    function showNavOnScroll(){
+        if(scrollY > 0){
+            document.querySelector('#navigation').classList.add('scroll')  
+        }
+        else{
+            document.querySelector('#navigation').classList.remove('scroll')  
+        }
     }
-}
+
+/*A Function showBackToTopButtonOnScroll possui a responsabilidade de mostrar o ícone do botão para subir para o ínicio da página*/
+    function showBackToTopButtonOnScroll(){
+        if(scrollY > 400){
+            document.querySelector('#backToTopButton').classList.add('show')  
+        }
+        else{
+            document.querySelector('#backToTopButton').classList.remove('scroll')  
+        }
     
-// Menu funções 
+    }
+    
+//Function Menu expandido ao clikar
 
 function openMenu(){
   document.querySelector('body').classList.add('menu-expanded')
@@ -21,28 +48,10 @@ function closeMenu(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* ScrollReveal lib*/
 
 ScrollReveal({
     origin: 'top',  //Ele vai começar a agir com a biblioteca no top - de cima para baixo
     distance: '30px',
     duration: 700,
-}).reveal("#home, #home img, #home .stats, #services, #services header, #services .card, #services .circute-icon, #about, #about header,#about p,#about img");
+}).reveal("#home, #home img, #home .stats, #services, #services header, #services .card, #services .circute-icon, #about, #about header,#about p,#about img, #contact,#contact p, #contact h2,#contact ul,#contact a");
