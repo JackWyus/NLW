db22e93dee6067615ae4dd61c1e4032d7c7f6c63
Vinicios Macario <JackWyus_01@outlook.com>
2022-06-13 21:13:59 -0300

    New update

--- js/main.js
+++ js/main.js
@@ -14,38 +14,39 @@ onScroll()
         showNavOnScroll();
         showBackToTopButtonOnScroll();
         activateMenuAtCurrentSection(home);
+        activateMenuAtCurrentSection(services);
+        activateMenuAtCurrentSection(about);
+        activateMenuAtCurrentSection(contact);
+
+
     }
 
-
-
-
-
-
-function activateMenuAtCurrentSection(section){
-    /*Linha Alvo/imaginária*/
-    const targetLine = (scrollY + (innerHeight / 2)); 
-
-    //Verificar se a seção passou da linha
-    //Quais dados vou precisar?
-
-    //O topo da seção
-    const sectionTop = section.offsetTop;
-    //A altura da seção
-    const sectionHeight = section.offsetHeight;
-
-    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
-
-    // A seção termina onde?
-    const sectionEndAt = sectionTop + sectionHeight;
-
-    //O final da seção passou a linha alvo
-    const sectionEndPassedTargetLine = sectionEndAt <= targetLine;
-
-    //Limites da seção
-    const sectionBoundaries = sectionEndPassedTargetLine && !sectionEndPassedTargetLine 
-
-
-}
+    function activateMenuAtCurrentSection(section) {
+        const targetLine = scrollY + innerHeight / 2
+      
+        // verificar se a seção passou da linha
+        // quais dados vou precisar?
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+      
+        // verificar se a base está abaixo da linha alvo
+      
+        const sectionEndsAt = sectionTop + sectionHeight
+        const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+      
+        // limites da seção
+        const sectionBoundaries =
+          sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+      
+        const sectionId = section.getAttribute('id')
+        const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+      
+        menuElement.classList.remove('active')
+        if (sectionBoundaries) {
+          menuElement.classList.add('active')
+        }
+      }
 
 
 
